65b5b2d10a63dfc1e4b97f517bc2481e50c05a90
drumm100 <marcelo_drumm@hotmail.com>
2018-06-10 17:48:07 -0300

    Começo da implementação.

--- api/Cidadao.java
+++ api/Cidadao.java
@@ -11,7 +11,12 @@ public class Cidadao implements Contribuinte
 
 
 
-    public Cidadao(){}
+    public Cidadao(int id, int idade, String nome)
+    {
+        this.id = id;
+        this.idade = idade;
+        this.nome = nome;
+    }
 
 
     @Override
